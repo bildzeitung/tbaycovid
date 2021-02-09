b67bef8cfe88145b99515e27675af6fa4b44a7a3
bildzeitung <bildzeitung@gmail.com>
2021-02-09 02:31:39 -0500

    cleanup

--- canada/canada/pipelines.py
+++ canada/canada/pipelines.py
@@ -20,23 +20,3 @@ class CanadaPipeline:
             fragment = f"{aircode(adapter['flight'])} from {aircode(adapter['depart'])} to {aircode(adapter['dest'])} on {adapter['date']}, rows {adapter['rows']}"
             return {"fragment": fragment}
         raise DropItem("Not relevant")
-
-    '''
-    def close_spider(self, spider):
-        # 4 flights per tweet
-        
-        #print(self._item_cache)
-        #print([len(x) for x in self._item_cache])
-        #print(sum(len(x) for x in self._item_cache))
-        def chunks():
-            for i in range(0, len(self._item_cache), self._chunks):
-                yield self._item_cache[i:i+self._chunks]
-
-        all_chunks = [c for c in chunks()]
-        l = len(all_chunks)
-        for x, c in enumerate(all_chunks):
-            f = '\n'.join(c)
-            s = f"[{x+1}/{l}] Flights with potential COVID exposure:\n{f}\n@TBDHealthUnit  @CityThunderBay"
-            print(s)
-            print(len(s))
-    '''
